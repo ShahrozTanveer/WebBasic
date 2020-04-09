eab11d4f7977d73b9afa1548728ec2e94fd8dc21
SharozTanveer <saadtanveer7@live.co.uk>
2020-04-10 01:52:18 +0500

    Day-5 Query selector js

--- assets/script.js
+++ assets/script.js
@@ -15,7 +15,11 @@ let x=1
 let y=2
 let number= x+y 
 console.log(number);
-
+//objects
+let obj= {
+    name: "abdullah",
+    age:21
+}
 
 //if conditions
 let n =10
@@ -33,9 +37,15 @@ if(n!=n2){
 else{
     console.log("false");
 }
-
-let i=10
+// ==  checks values not data types
+let i=10 
 let j="10"
+console.log("checking type");
+console.log(typeof(i));
+console.log(typeof(j));
+
+
+
 if(i==j){
     console.log("true ==");
 }
@@ -85,7 +95,7 @@ function addTwoNumbers(a,b){
 let number1=4;
 let number2=5;
 let retNumber=addTwoNumbers(number1,number2)
-alert(`number a is ${number1} and b is ${number2} and result is ${retNumber}`) // Template string
+// alert(`number a is ${number1} and b is ${number2} and result is ${retNumber}`) // Template string
 
 const valueOfPi = Math.PI
 console.log(valueOfPi);
@@ -101,3 +111,32 @@ console.log(Math.min(1,20,4));
 // python
 // for e in arr:
 //     print(e)
+
+
+//dom 
+function buttonPressed(){
+    console.log("you pressed button")
+    alert("hey there.")
+}
+const target = document.querySelector("body > div.content > a") 
+
+target.addEventListener('click', buttonPressed)
+let flag= true
+function headerClicked() {
+
+    const e= document.querySelector("body .content h1")
+    if(flag){
+    e.innerHTML="This is Header"
+    e.style.backgroundColor="#eee"
+    flag=false
+    }
+    else {
+        e.style.backgroundColor="rgb(66, 62, 62)"
+        e.innerHTML="This is H1"
+        flag=true
+    }
+}
+
+const header_element= document.querySelector("body .content h1")
+header_element.addEventListener('click',headerClicked )
+
